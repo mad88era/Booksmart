796c5aef81ca440589cd3fa7768018993771bb3f
mad88era <150746505+mad88era@users.noreply.github.com>
2024-06-12 17:50:50 -0700

    Add files via upload

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,43 @@
+import { gql } from "@apollo/client";
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+addUser(username:$username, email:$email, password:$password)
+{
+token
+user{
+_id
+username}
+}
+}
+`;
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+    login(email:$email, password:$password)
+    {
+    token
+    user{
+    _id
+    username}
+    }
+    }
+`;
+export const SAVE_BOOK = gql`
+mutation saveBook($newBook: BookInput){
+saveBook(newBook:$newBook)
+{
+_id
+username
+savedBooks{
+title
+authors
+image}}}`;
+
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: ID!)
+{
+removeBook(bookId:$bookId)
+{
+_id
+username
+email}}`;
+
